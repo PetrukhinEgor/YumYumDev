6fd54e189bc4b8d43a17b435e22f8cb44dd58f22
Egor <PED.22@uni-dubna.ru>
2026-04-22 21:04:39 +0300

    оформил общий репозиторий, обновил readme

--- YumYum-app/config/api.js
+++ YumYum-app/config/api.js
@@ -1,7 +1,7 @@
 // YumYum-app/config/api.js
 
 //интернет через универ
-// export const API_URL = "http://172.20.10.5:5000"; 
+// export const API_URL = "http://10.221.254.111:5000"; 
 
 //интернет через wifi
 export const API_URL = "http://192.168.1.138:5000"; 
